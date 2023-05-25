d590aea0aefa05d09895c1972f95f15356260d25
EstebanBitL <esteban.lescano@bitlogic.io>
2023-05-25 13:22:41 -0300

    test position

--- src/main/java/org/example/MarsRover.java
+++ src/main/java/org/example/MarsRover.java
@@ -1,2 +1,106 @@
-package org.example;public class MarsRover {
+package org.example;
+public class MarsRover {
+    private int x;  // Posición en el eje x
+    private int y;  // Posición en el eje y
+    private char direction;  // Dirección (N, S, E, O)
+
+    public MarsRover(int x, int y, char direction) {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+
+    public boolean checkPosition(int x, int y, char direction) {
+        return this.x == x && this.y == y && this.direction == direction;
+    }
+
+    public void processCommands(String commands) {
+        for (char command : commands.toCharArray()) {
+            switch (command) {
+                case 'F':  // Mover hacia adelante
+                    moveForward();
+                    break;
+                case 'B':  // Mover hacia atrás
+                    moveBackward();
+                    break;
+                case 'L':  // Girar a la izquierda
+                    turnLeft();
+                    break;
+                case 'R':  // Girar a la derecha
+                    turnRight();
+                    break;
+                default:
+                    // Comando inválido, puedes lanzar una excepción o manejarlo según tus necesidades
+                    break;
+            }
+        }
+    }
+
+    private void moveForward() {
+        switch (direction) {
+            case 'N':
+                y++;
+                break;
+            case 'S':
+                y--;
+                break;
+            case 'E':
+                x++;
+                break;
+            case 'W':
+                x--;
+                break;
+        }
+    }
+
+    private void moveBackward() {
+        switch (direction) {
+            case 'N':
+                y--;
+                break;
+            case 'S':
+                y++;
+                break;
+            case 'E':
+                x--;
+                break;
+            case 'W':
+                x++;
+                break;
+        }
+    }
+
+    private void turnLeft() {
+        switch (direction) {
+            case 'N':
+                direction = 'W';
+                break;
+            case 'S':
+                direction = 'E';
+                break;
+            case 'E':
+                direction = 'N';
+                break;
+            case 'W':
+                direction = 'S';
+                break;
+        }
+    }
+
+    private void turnRight() {
+        switch (direction) {
+            case 'N':
+                direction = 'E';
+                break;
+            case 'S':
+                direction = 'W';
+                break;
+            case 'E':
+                direction = 'S';
+                break;
+            case 'W':
+                direction = 'N';
+                break;
+        }
+    }
 }
--- src/test/java/org/example/MarsRoverTest.java
+++ src/test/java/org/example/MarsRoverTest.java
@@ -1,7 +1,146 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class MarsRoverTest {
+public class MarsRoverTest {
 
+    @Test
+    public void testInitialPositionAndDirection() {
+        MarsRover rover = new MarsRover(1, 1, 'N');
+        assertTrue(rover.checkPosition(1, 1, 'N'));
+    }
+
+    @Test
+    public void testCheckPosition() {
+        MarsRover rover = new MarsRover(2, 3, 'S');
+
+        assertTrue(rover.checkPosition(2, 3, 'S'));
+        assertFalse(rover.checkPosition(1, 1, 'N'));
+        assertFalse(rover.checkPosition(1,1,'W'));
+        assertFalse(rover.checkPosition(1,1, 'O'));
+    }
+
+    @Test
+    public void testMoveForward() {
+        MarsRover rover = new MarsRover(0, 0, 'N');
+        rover.processCommands("FF");//adelante ++
+
+        assertTrue(rover.checkPosition(0, 2, 'N'));
+
+        rover = new MarsRover(0, 0, 'S');
+        rover.processCommands("F");//adelante
+
+        assertTrue(rover.checkPosition(0, -1, 'S'));
+
+        rover = new MarsRover(0, 0, 'W');
+        rover.processCommands("FL");//adelante izquier
+
+        assertTrue(rover.checkPosition(-1,0,'S'));
+
+        rover = new MarsRover(0, 0, 'E');
+        rover.processCommands("FR");//adelante der
+
+        assertTrue(rover.checkPosition(1,0,'S'));
+    }
+
+    @Test
+    public void testMoveBackward() {
+        MarsRover rover = new MarsRover(0, 0, 'N');
+        rover.processCommands("BB");
+        assertTrue(rover.checkPosition(0, -2, 'N'));
+
+        rover = new MarsRover(0, 0, 'S');
+        rover.processCommands("B");
+        assertTrue(rover.checkPosition(0, 1, 'S'));
+    }
+
+    @Test
+    public void testTurnLeft() {
+        MarsRover rover = new MarsRover(0, 0, 'N');
+        rover.processCommands("L");
+        assertTrue(rover.checkPosition(0, 0, 'W'));
+
+        rover = new MarsRover(0, 0, 'E');
+        rover.processCommands("LLL");
+        assertTrue(rover.checkPosition(0, 0, 'S'));
+    }
+
+    @Test
+    public void testTurnRight() {
+        //va a la direccion este desde la posicion 0
+        MarsRover rover = new MarsRover(0, 0, 'N');
+        rover.processCommands("R");
+        assertTrue(rover.checkPosition(0, 0, 'E'));
+
+        //va a la direccion oeste desde la posicion 0 queda mirando al sur
+        rover = new MarsRover(0, 0, 'W');
+        rover.processCommands("RRR");
+        assertTrue(rover.checkPosition(0, 0, 'S'));
+    }
+
+    @Test
+    public void testCadenaDeComandos_1(){
+        MarsRover rover = new MarsRover(1, 1, 'N');
+        rover.processCommands("FB");
+        //hace adelante atras pero siempre queda mirando al norte camino feliz
+        assertTrue(rover.checkPosition(1,1,'N'));
+        //hace adelante atras pero siempre queda mirando al norte camino triste
+        assertFalse(rover.checkPosition(1,1,'S'));
+    }
+
+    @Test
+    public void testCadenaDeComandos_2(){
+        MarsRover rover = new MarsRover(1, 1, 'N');
+        rover.processCommands("LFR");
+        //le paso izq,adel,der queda mirando al norte
+        assertTrue(rover.checkPosition(0,1,'N'));
+    }
+
+    @Test
+    public void testCadenaDeComandos_3FF(){
+        MarsRover rover = new MarsRover(1, 1, 'N');
+        rover.processCommands("FF");
+        //cadena complicada tuve que imitar el rover
+        assertTrue(rover.checkPosition(1,3,'N'));
+    }
+    @Test
+    public void testCadenaDeComandos_3FFR(){
+        MarsRover rover = new MarsRover(1, 1, 'N');
+        rover.processCommands("FFR");
+        assertTrue(rover.checkPosition(1,3,'E'));
+    }
+    @Test
+    public void testCadenaDeComandos_3FFRFF(){
+        MarsRover rover = new MarsRover(1, 1, 'N');
+        rover.processCommands("FFRFF");
+        assertTrue(rover.checkPosition(3,3,'E'));
+    }
+    @Test
+    public void testCadenaDeComandos_3FFRFFL(){
+        MarsRover rover = new MarsRover(1, 1, 'N');
+        rover.processCommands("FFRFFL");
+        assertTrue(rover.checkPosition(3,3,'N'));
+    }
+    @Test
+    public void testCadenaDeComandos_3FFRFFLF(){
+        MarsRover rover = new MarsRover(1, 1, 'N');
+        rover.processCommands("FFRFFLF");
+        assertTrue(rover.checkPosition(3,4,'N'));
+    }
+    @Test
+    public void testCadenaDeComandos_3FFRFFLFR(){
+        MarsRover rover = new MarsRover(1, 1, 'N');
+        rover.processCommands("FFRFFLFR");
+        assertTrue(rover.checkPosition(3,4,'E'));
+    }
+
+    @Test
+    public void testCadenaDeComandos_3FFRFFLFRFF(){
+        MarsRover rover = new MarsRover(1, 1, 'N');
+        rover.processCommands("FFRFFLFRFF");
+        //cadena complicada tuve que imitar el rover
+        assertTrue(rover.checkPosition(5,4,'E'));
+    }
 }
